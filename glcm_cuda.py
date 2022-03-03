02f83465a3d013f71cbe4879af6e981b661da3e4
Evening <johnchangqi@hotmail.com>
2022-03-03 13:55:05 +0800

    Migrate to use class variables

--- glcm_cuda.py
+++ glcm_cuda.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 import cupy as cp
 import numpy as np
 from skimage.util import view_as_windows
@@ -7,11 +9,26 @@ from tqdm import tqdm
 
 BLOCKS = 256
 THREADS = 256
-MAX_VALUE_SUPPORTED = 256
+MAX_VALUE_SUPPORTED = 255
 NO_VALUES_SUPPORTED = 256 ** 2
 
-
+@dataclass
 class GLCM:
+    """
+    
+    Args:
+        max_value: Maximum value of the image, default 256
+        step_size: Step size of the window
+        radius: Radius of the windows
+        bins: Bin reduction. If None, then no reduction is done
+
+    """
+    
+    max_value: int = 255
+    step_size: int = 1
+    radius: int = 2
+    bins: int | None = None
+
     HOMOGENEITY = 0
     CONTRAST = 1
     ASM = 2
@@ -55,19 +72,20 @@ class GLCM:
         return np.stack(glcm_chs, axis=2)
 
     def from_image(self,
-                   step_size: int = 1,
-                   radius: int = 2) -> np.ndarray:
-        """ Generates the GLCM from an image
+                   im: np.ndarray) -> np.ndarray:
+        """ Generates the GLCM from a single band image
 
         Args:
             im: Image in np.ndarray. Cannot be in cp.ndarray
-            step_size: Step size of the window
-            radius: Radius of the windows
 
         Returns:
-            The GLCM Array
+            The GLCM Array 3dim with shape
+                rows, cols, feature
         """
-        diameter = radius * 2 + 1
+        diameter = self.radius * 2 + 1
+
+        if self.bins is not None:
+            im = self.binarize(im, self.max_value, self.bins)
 
         # This will yield a shape (window_i, window_j, row, col)
         # E.g. 100x100 with 5x5 window -> 96, 96, 5, 5
@@ -77,24 +95,23 @@ class GLCM:
         windows_ij = windows_ij.reshape((*windows_ij.shape[:-2], -1))
 
         # Yield Windows and flatten the windows
-        windows_i = windows_ij[:-step_size, :-step_size] \
+        windows_i = windows_ij[:-self.step_size, :-self.step_size] \
             .reshape((-1, windows_ij.shape[-1]))
-        windows_j = windows_ij[step_size:, step_size:] \
+        windows_j = windows_ij[self.step_size:, self.step_size:] \
             .reshape((-1, windows_ij.shape[-1]))
 
         glcm_features = cp.zeros((windows_i.shape[0], 8), dtype=cp.float32)
 
         for e, (i, j) in tqdm(enumerate(zip(windows_i, windows_j)),
                               total=len(windows_i)):
-            glcm_features[e] = self.from_windows(i, j)
+            glcm_features[e] = self._from_windows(i, j)
 
-        return glcm_features.reshape(windows_ij.shape[0] - step_size,
-                                     windows_ij.shape[1] - step_size, 8)
+        return glcm_features.reshape(windows_ij.shape[0] - self.step_size,
+                                     windows_ij.shape[1] - self.step_size, 8)
 
-    def from_windows(self,
-                     i: cp.ndarray,
-                     j: cp.ndarray,
-                     max_value: None | int = None) -> np.ndarray:
+    def _from_windows(self,
+                      i: cp.ndarray,
+                      j: cp.ndarray, ) -> np.ndarray:
         """ Generate the GLCM from the I J Window
 
         Notes:
@@ -103,22 +120,19 @@ class GLCM:
         Args:
             i: I Window
             j: J Window
-            max_value: The maximum possible value. If None, it'll be calculated
-                automatically.
 
         Returns:
             The GLCM array, of size (8,)
 
         """
-        max_value = max_value if max_value else int(max(cp.max(i), cp.max(j)))
         assert i.shape == j.shape, f"Shape of i {i.shape} != j {j.shape}"
         i_flat = i.flatten()
         j_flat = j.flatten()
         no_of_values = i_flat.size
-        glcm = cp.zeros((max_value + 1) ** 2, dtype=cp.uint8).flatten()
+        glcm = cp.zeros((self.max_value + 1) ** 2, dtype=cp.uint8).flatten()
         features = cp.zeros(8, dtype=cp.float32)
 
-        assert max_value < MAX_VALUE_SUPPORTED, \
+        assert self.max_value <= MAX_VALUE_SUPPORTED, \
             f"Max value supported is {MAX_VALUE_SUPPORTED}"
         assert no_of_values < NO_VALUES_SUPPORTED, \
             f"Max number of values supported is {NO_VALUES_SUPPORTED}"
@@ -127,9 +141,10 @@ class GLCM:
             grid=(256,),
             block=(256,),
             args=(
-                i_flat, j_flat, max_value, no_of_values, glcm, features
+                i_flat, j_flat, self.max_value, no_of_values, glcm, features
             ),
         )
+
         return features
 
     @property

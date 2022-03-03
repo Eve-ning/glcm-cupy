6cdef9cdefceee7882b999b3a4fcc864d2cf0528
Evening <johnchangqi@hotmail.com>
2022-03-03 16:22:40 +0800

    Update test to include fragment tests

--- tests/unit_tests/test_glcm.py
+++ tests/unit_tests/test_glcm.py
@@ -14,6 +14,24 @@ from glcm_cuda import GLCM
         simple_0, simple_1
     ]
 )
-def test_glcm_asm(i, j):
+def test_glcm(
+        i, j,
+        homogeneity,
+        contrast,
+        asm,
+        mean_i,
+        mean_j,
+        var_i,
+        var_j,
+        correlation
+):
     g = GLCM()._from_windows(i, j)
-    assert g[..., GLCM.ASM].sum() == 1
+
+    assert g[..., GLCM.HOMOGENEITY].sum() == homogeneity
+    assert g[..., GLCM.CONTRAST].sum() == contrast
+    assert g[..., GLCM.ASM].sum() == asm
+    assert g[..., GLCM.MEAN_I].sum() == mean_i
+    assert g[..., GLCM.MEAN_J].sum() == mean_j
+    assert g[..., GLCM.VAR_I].sum() == var_i
+    assert g[..., GLCM.VAR_J].sum() == var_j
+    assert g[..., GLCM.CORRELATION].sum() == correlation

36d4523939c92df160a6a74985cd33fe42162325
Evening <johnchangqi@hotmail.com>
2022-03-22 00:25:57 +0800

    Migrate Test to master test file

--- tests/unit_tests/test_binarize.py
+++ tests/unit_tests/test_binarize.py
@@ -5,14 +5,3 @@ import pytest
 from glcm_cuda import GLCM
 from tests.unit_tests import glcm_expected
 
-
-@pytest.mark.parametrize(
-    "bins",
-    [2, 3, 256]
-)
-def test_binarize(i, j):
-    g = GLCM.binarize(np.asarray([0, 1, 2], dtype=np.uint8), 2, 2)
-
-    expected = glcm_expected(i, j)
-    assert actual == pytest.approx(expected, abs=1e-2)
-

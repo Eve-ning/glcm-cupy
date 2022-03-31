262f2ba056e600709b5d39f43b5fa01a3dab2266
Evening <johnchangqi@hotmail.com>
2022-03-31 22:10:28 +0800

    Expose common variables

--- glcm_cupy/__init__.py
+++ glcm_cupy/__init__.py
@@ -1,5 +1,16 @@
+from .conf import HOMOGENEITY, CONTRAST, ASM, MEAN_I, MEAN_J, VAR_I, VAR_J, CORRELATION
 from .glcm import GLCM
+from .windowing import Direction
 
 __all__ = [
     "GLCM",
+    "HOMOGENEITY",
+    "CONTRAST",
+    "ASM",
+    "MEAN_I",
+    "MEAN_J",
+    "VAR_I",
+    "VAR_J",
+    "CORRELATION",
+    "Direction"
 ]

94ef8390323df8de36bcb7568903e8b86baf18bd
Evening <johnchangqi@hotmail.com>
2022-04-01 21:11:40 +0800

    Expose helper im_shape_after_glcm

--- glcm_cupy/__init__.py
+++ glcm_cupy/__init__.py
@@ -1,6 +1,6 @@
 from .conf import HOMOGENEITY, CONTRAST, ASM, MEAN_I, MEAN_J, VAR_I, VAR_J, CORRELATION
 from .glcm import GLCM, glcm
-from .windowing import Direction
+from .windowing import Direction, im_shape_after_glcm
 
 __all__ = [
     "glcm",
@@ -13,5 +13,6 @@ __all__ = [
     "VAR_I",
     "VAR_J",
     "CORRELATION",
-    "Direction"
+    "Direction",
+    "im_shape_after_glcm"
 ]

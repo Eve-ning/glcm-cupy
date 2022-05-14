26c2221d2743362dd472eb0df22506da9036bdd3
Evening <johnchangqi@hotmail.com>
2022-05-14 22:28:30 +0800

    Attempt to implement GLCM Cross

--- glcm_cupy/cross/glcm_cross.py
+++ glcm_cupy/cross/glcm_cross.py
@@ -1,31 +1,34 @@
 from __future__ import annotations
 
-import math
-from typing import Tuple, Iterable
+import itertools
+from dataclasses import dataclass
+from enum import Enum
+from typing import Tuple, List
 
 import cupy as cp
 import numpy as np
 from skimage.util import view_as_windows
-from tqdm import tqdm
 
 from glcm_cupy.conf import *
-from glcm_cupy.kernel import glcm_module
-from glcm_cupy.windowing import glcm_shape, Direction
+from glcm_cupy.glcm_base import GLCMBase
 
 
-def glcm(
+class Direction(Enum):
+    EAST = 0
+    SOUTH_EAST = 1
+    SOUTH = 2
+    SOUTH_WEST = 3
+
+
+def glcm_cross(
     im: np.ndarray,
-    step_size: int = 1,
     radius: int = 2,
     bin_from: int = 256,
     bin_to: int = 256,
-    directions: Iterable[Direction] = (Direction.EAST,
-                                       Direction.SOUTH_EAST,
-                                       Direction.SOUTH,
-                                       Direction.SOUTH_WEST),
     max_partition_size: int = MAX_PARTITION_SIZE,
     max_threads: int = MAX_THREADS,
-    normalize_features: bool = True) -> np.ndarray:
+    normalize_features: bool = True
+) -> np.ndarray:
     """
     Examples:
         To scale down the image from a 128 max value to 32, we use
@@ -37,123 +40,41 @@ def glcm(
 
     Args:
         im: Image to Process
-        step_size: Stride Between GLCMs
         radius: Radius of Window
         bin_from: Binarize from.
         bin_to: Binarize to.
-        directions: Directions to pair the windows.
         max_partition_size: Maximum number of windows to parse at once
+        max_threads: Maximum threads for CUDA
         normalize_features: Whether to normalize features to [0, 1]
 
     Returns:
-
-
+        GLCM Features
     """
     return GLCMCross(step_size, radius, bin_from, bin_to,
-                directions, max_partition_size, max_threads,
-                normalize_features).run(im)
+                     max_partition_size, max_threads,
+                     normalize_features).run(im)
 
 
-class GLCMCross:
-    def __init__(self,
-                 step_size: int = 1,
-                 radius: int = 2,
-                 bin_from: int = 256,
-                 bin_to: int = 256,
-                 directions: Iterable[Direction] = (Direction.EAST,
-                                                    Direction.SOUTH_EAST,
-                                                    Direction.SOUTH,
-                                                    Direction.SOUTH_WEST),
-                 max_partition_size: int = MAX_PARTITION_SIZE,
-                 max_threads: int = MAX_THREADS,
-                 normalize_features: bool = True
-                 ):
-        """ Initialize Settings for GLCM
+@dataclass
+class GLCMCross(GLCMBase):
 
-        Examples:
-            To scale down the image from a 128 max value to 32, we use
-            bin_from = 128, bin_to = 32.
+    def glcm_cells(self, im: np.ndarray) -> float:
+        shape = self.glcm_shape(im)
+        # TODO: * by number of combinations
+        return np.prod(shape) * 1
 
-            The range will collapse from 128 to 32.
-
-            This thus optimizes the GLCM speed.
-
-        Args:
-            step_size: Stride Between GLCMs
-            radius: Radius of Window
-            bin_from: Binarize from.
-            bin_to: Binarize to.
-            directions: Directions to pair the windows.
-            max_partition_size: Maximum number of windows to parse at once
-            max_threads: Maximum number of threads to use per block
-            normalize_features: Whether to normalize features to [0, 1]
-        """
-        self.step_size = step_size
-        self.radius = radius
-        self.bin_from = bin_from
-        self.bin_to = bin_to
-        self.directions = directions
-        self.max_partition_size = max_partition_size
-        self.max_threads = max_threads
-        self.normalize_features = normalize_features
-        self.progress = None
-
-        self.glcm = cp.zeros(
-            (self.max_partition_size, self.bin_to, self.bin_to),
-            dtype=cp.float32)
-        self.features = cp.zeros((self.max_partition_size, NO_OF_FEATURES),
-                                 dtype=cp.float32)
-        if self.radius < 0:
-            raise ValueError(
-                f"Radius {radius} should be > 0)"
-            )
-        if bin_to not in range(2, MAX_VALUE_SUPPORTED + 1):
-            raise ValueError(
-                f"Target Bins {bin_to} should be "
-                f"[2, {MAX_VALUE_SUPPORTED}]. "
-            )
-        if self.step_size <= 0:
-            raise ValueError(
-                f"Step Size {step_size} should be >= 1"
-            )
-
-        self.glcm_create_kernel = \
-            glcm_module.get_function('glcmCreateKernel')
-        self.glcm_feature_kernel_0 = \
-            glcm_module.get_function('glcmFeatureKernel0')
-        self.glcm_feature_kernel_1 = \
-            glcm_module.get_function('glcmFeatureKernel1')
-        self.glcm_feature_kernel_2 = \
-            glcm_module.get_function('glcmFeatureKernel2')
-
-    @property
-    def _diameter(self):
-        return self.radius * 2 + 1
-
-    def run(self, im: np.ndarray):
-        """ Executes running GLCM. Returns the GLCM Feature array
-
-        Args:
-            im: Image to process. Can be 2D or 3D.
+    def glcm_shape(self, im: np.ndarray):
+        """ Calculate the image shape after GLCM
 
         Returns:
-            An np.ndarray of Shape,
-             3D: (rows, cols, channels, features),
-             2D: (rows, cols, features)
-
+            Shape of Image after GLCM
         """
-        shape = glcm_shape(im.shape, self.step_size, self.radius)
-        glcm_cells = np.prod(shape) * len(self.directions)
-        self.progress = tqdm(total=glcm_cells, desc="GLCM Progress",
-                             unit=" Cells",
-                             unit_scale=True)
 
-        if im.ndim == 2:
-            return self._from_2dimage(im)
-        elif im.ndim == 3:
-            return self._from_3dimage(im)
-        else:
-            raise ValueError("Only 2D or 3D images allowed.")
+        shape = im.shape[:2]
+
+        return (shape[0] - 2 * self.radius,
+                shape[1] - 2 * self.radius,
+                *shape[2:])
 
     def _from_3dimage(self, im: np.ndarray) -> np.ndarray:
         """ Generates the GLCM from a multi band image
@@ -166,206 +87,51 @@ class GLCMCross:
                 rows, cols, channel, feature
         """
 
-        glcm_chs = []
-        for ch in range(im.shape[-1]):
-            glcm_chs.append(self._from_2dimage(im[..., ch]))
+        combos = list(itertools.combinations(range(im.shape[-1]), 2))
+        glcm_chs = [
+            self._from_2dimage(im[..., combo]) for combo in combos
+        ]
 
         return np.stack(glcm_chs, axis=2)
 
-    def _from_2dimages(self,
-                       im_i: np.ndarray,
-                       im_j: np.ndarray) -> np.ndarray:
-        """ Generates the Cross GLCM from a 2 2D images
-
-        Notes:
-            Partitions the image into blocks of PARTITION_SIZE,
-            reducing allocated GLCM Memory.
+    def make_windows(self, im: np.ndarray) -> \
+        List[Tuple[np.ndarray, np.ndarray]]:
+        """ Convert 3D image np.ndarray with 2 channels to IJ windows.
 
         Args:
-            im: Image in np.ndarray.
+            im: Input Image. Must be of shape (h, w, 2)
 
         Returns:
-            The GLCM Array 3dim with shape
-                rows, cols, feature
-        """
+            A List of I, J windows based on the directions.
 
-        if im_i.ndim != 2 or im_j.ndim != 2:
-            raise ValueError(f"Images must be 2 dimensional. "
-                             f"im_i.ndim={im_i.ndim}, im_j.ndim={im_i.ndim}")
-
-        im0 = self._binner(im_i, self.bin_from, self.bin_to)
-        im1 = self._binner(im_j, self.bin_from, self.bin_to)
-
-        windows_i = view_as_windows(im0, (self._diameter, self._diameter))
-        windows_j = view_as_windows(im1, (self._diameter, self._diameter))
-
-        windows_count = windows_i.shape[0]
-        glcm_features = cp.zeros(
-            (windows_count, NO_OF_FEATURES),
-            dtype=cp.float32
-        )
-
-        # If 45000 windows, 10000 partition size,
-        # We have 5 partitions (10K, 10K, 10K, 10K, 5K)
-        partition_count = math.ceil(windows_count / self.max_partition_size)
-
-        for partition in range(partition_count):
-            with cp.cuda.Device() as dev:
-                # As above, we have an uneven partition
-                # Though [1,2,3][:5] == [1,2,3]
-                # This means windows_part_i will be correctly partitioned.
-                start = partition * self.max_partition_size
-                end = (partition + 1) * self.max_partition_size
-                part_i = windows_i[start:end]
-                part_j = windows_j[start:end]
-
-                # We don't need to figure out the leftover partition size
-                # We can simply yield the length of the leftover partition
-                windows_part_count = part_i.shape[0]
-
-                self.glcm[:] = 0
-                glcm_features[start:start + windows_part_count] = \
-                    self.run_ij(part_i, part_j)
-                dev.synchronize()
-                self.progress.update(windows_part_count)
-
-        ar = glcm_features.reshape(im_i.shape[0],
-                                   im_j.shape[1],
-                                   NO_OF_FEATURES).get()
-
-        if self.normalize_features:
-            # This scales the glcm features to [0, 1]
-            ar[..., CONTRAST] /= (self.bin_to - 1) ** 2
-            ar[..., MEAN] /= (self.bin_to - 1)
-            ar[..., VAR] /= (self.bin_to - 1) ** 2
-            ar[..., CORRELATION] += 1
-            ar[..., CORRELATION] /= 2
-        return ar
-
-    def run_ij(self,
-               i: np.ndarray,
-               j: np.ndarray):
-        """ Generate the GLCM from the I J Window
-
-        Examples:
-
-            >>> ar_0 = np.random.randint(0, 100, 10, dtype=np.uint8)
-            >>> ar_1 = np.random.randint(0, 100, 10, dtype=np.uint8)
-            >>> g = GLCM().glcm_single(ar_0[...,np.newaxis], ar_1[...,np.newaxis])
-
-        Notes:
-            i must be the same shape as j
-            i.shape, j.shape should be [partition_size, window_size]
-
-            For example, if you want to process 100 windows at once,
-            each window with 25 cells, you should have
-            i.shape == j.shape == (100, 25)
-
-        Args:
-            i: 1st np.ndarray
-            j: 2nd np.ndarray
-
-        Returns:
-            The GLCM feature array, of size [partition_size, 8]
+            For each window:
+                The first dimension: xy flat window indexes,
+                the last dimension: xy flat indexes within each window.
 
         """
-        if i.ndim != 2 or j.ndim != 2:
+        if im.ndim != 3:
+            raise ValueError(f"Image must be 3 dimensional. im.ndim={im.ndim}")
+
+        glcm_h, glcm_w, *_ = self.glcm_shape(im)
+        if glcm_h <= 0 or glcm_w <= 0:
             raise ValueError(
-                f"The input dimensions must be 2. "
-                f"i.ndim=={i.ndim}, j.ndim=={j.ndim}. "
-                "The 1st Dim is the partitioned windows flattened, "
-                "The 2nd is the window cells flattened"
+                f"Step Size & Diameter exceeds size for windowing. "
+                f"im.shape[0] {im.shape[0]} "
+                f"- 2 * step_size {step_size} "
+                f"- 2 * radius {radius} <= 0 or"
+                f"im.shape[1] {im.shape[1]} "
+                f"- 2 * step_size {step_size} "
+                f"- 2 * radius {radius} + 1 <= 0 was not satisfied."
             )
 
-        if i.shape != j.shape:
-            raise ValueError(f"Shape of i {i.shape} != j {j.shape}")
+        i = cp.asarray(
+            view_as_windows(im[..., 0], (self._diameter, self._diameter)))
+        j = cp.asarray(
+            view_as_windows(im[..., 1], (self._diameter, self._diameter)))
 
-        # Reset data
-        self.glcm[:] = 0
-        self.features[:] = 0
+        i = i.reshape((-1, *i.shape[-2:]))\
+            .reshape((i.shape[0] * i.shape[1], -1))
+        j = j.reshape((-1, *j.shape[-2:]))\
+            .reshape((j.shape[0] * j.shape[1], -1))
 
-        no_of_windows = i.shape[0]
-        no_of_values = self._diameter ** 2
-
-        if i.dtype != np.uint8 or j.dtype != np.uint8:
-            raise ValueError(
-                f"Image dtype must be np.uint8,"
-                f" i: {i.dtype} j: {j.dtype}"
-            )
-
-        grid = self._calc_grid_size(no_of_windows,
-                                    self.bin_to,
-                                    self.max_threads)
-        self.glcm_create_kernel(
-            grid=grid,
-            block=(self.max_threads,),
-            args=(
-                i,
-                j,
-                self.bin_to,
-                no_of_values,
-                no_of_windows,
-                self.glcm,
-                self.features
-            )
-        )
-
-        feature_args = dict(
-            grid=grid, block=(self.max_threads,),
-            args=(self.glcm,
-                  self.bin_to,
-                  no_of_values,
-                  no_of_windows,
-                  self.features)
-        )
-        self.glcm_feature_kernel_0(**feature_args)
-        self.glcm_feature_kernel_1(**feature_args)
-        self.glcm_feature_kernel_2(**feature_args)
-
-        return self.features[:no_of_windows]
-
-    @staticmethod
-    def _calc_grid_size(
-        window_count: int,
-        glcm_size: int,
-        thread_per_block: int = MAX_THREADS
-    ) -> Tuple[int, int]:
-        """ Calculates the required grid size
-
-        Notes:
-            There's 2 points where the number of threads
-
-        Returns:
-            The optimal minimum grid shape for GLCM
-
-        """
-
-        # TODO: Do we need to div by thread_per_block?
-        # Blocks to support features
-        blocks_req_glcm_features = \
-            window_count * glcm_size * glcm_size / thread_per_block
-
-        # Blocks to support glcm populating
-        blocks_req_glcm_populate = glcm_size * window_count
-
-        # Take the maximum
-        blocks_req = max(blocks_req_glcm_features, blocks_req_glcm_populate)
-
-        # We split it to 2 dims: A -> B x B
-        b = int(blocks_req ** 0.5) + 1
-        return b, b
-
-    @staticmethod
-    def _binner(im: np.ndarray, bin_from: int, bin_to: int) -> np.ndarray:
-        """ Bins an image from a certain bin to another
-
-        Args:
-            im: Image as np.ndarray
-            bin_from: From the Bin of input image
-            bin_to: To the Bin of output image
-
-        Returns:
-            Binned Image
-
-        """
-        return (im.astype(np.float32) / bin_from * bin_to).astype(np.uint8)
+        return [(i, j)]

97a9b92460383a2f7deb39cd12ae651d169ae98a
Julio Faracco <jcfaracco@gmail.com>
2022-06-18 20:23:50 +0000

    test: fix deprecated scikit-image methods for GLCM
    Scikit-image deprecated both methods: skimage.feature.graycomatrix and
    skimage.feature.graycoprops. The project replaced them to
    skimage.feature.greycomatrix and skimage.feature.greycoprops. It is just
    a new name convention. That's why this commit just introduce an alias
    for older releases.
    
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- tests/unit_tests/util/glcm_py_skimage.py
+++ tests/unit_tests/util/glcm_py_skimage.py
@@ -1,18 +1,25 @@
 import numpy as np
-from skimage.feature import graycoprops, graycomatrix
+
+try:
+    # Try to import scikit-image 0.19+
+    from skimage.feature import greycoprops
+    from skimage.feature import greycomatrix
+except:
+    from skimage.feature import graycoprops as greycoprops
+    from skimage.feature import graycomatrix as greycomatrix
 
 
 def glcm_py_skimage(i, j):
     ar_ij = np.stack([i, j], axis=-1)
     ar_ji = np.stack([j, i], axis=-1)
-    glcm_ij = graycomatrix(ar_ij, (1,), (0,))
-    glcm_ji = graycomatrix(ar_ji, (1,), (0,))
+    glcm_ij = greycomatrix(ar_ij, (1,), (0,))
+    glcm_ji = greycomatrix(ar_ji, (1,), (0,))
     glcm = (glcm_ij + glcm_ji) / 2
 
-    contrast = float(graycoprops(glcm, 'contrast').squeeze())
-    homogeneity = float(graycoprops(glcm, 'homogeneity').squeeze())
-    asm = float(graycoprops(glcm, 'ASM').squeeze())
-    correlation = float(graycoprops(glcm, 'correlation').squeeze())
+    contrast = float(greycoprops(glcm, 'contrast').squeeze())
+    homogeneity = float(greycoprops(glcm, 'homogeneity').squeeze())
+    asm = float(greycoprops(glcm, 'ASM').squeeze())
+    correlation = float(greycoprops(glcm, 'correlation').squeeze())
 
     return dict(
         homogeneity=homogeneity,

a1ca2d3cc8c074f8f4c5bb6ef7134a70b1a82a71
Evening <johnchangqi@hotmail.com>
2022-07-03 17:21:17 +0800

    Update 4d tests

--- tests/unit_tests/test_glcm_cross_4d.py
+++ tests/unit_tests/test_glcm_cross_4d.py
@@ -2,7 +2,7 @@ import cupy as cp
 import numpy as np
 import pytest
 
-from glcm_cupy import GLCM
+from glcm_cupy import GLCMCross
 
 
 @pytest.mark.parametrize(
@@ -22,10 +22,9 @@ from glcm_cupy import GLCM
     [1, 2, 10]
 )
 def test_glcm_4d(size, bins, radius, batches):
-    """ Tests the case where a batch is sent. """
     np.random.seed(0)
-    ar_batch = np.random.randint(0, bins, [batches, size, size, 1])
-    glcm = GLCM(radius=radius, bin_from=bins, bin_to=bins)
+    ar_batch = np.random.randint(0, bins, [batches, size, size, 3])
+    glcm = GLCMCross(radius=radius, bin_from=bins, bin_to=bins)
     g_batch = glcm.run(ar_batch)
     g = np.stack([glcm.run(ar) for ar in ar_batch])
     assert g == pytest.approx(g_batch, abs=1e-06)
@@ -48,10 +47,9 @@ def test_glcm_4d(size, bins, radius, batches):
     [1, 2, 10]
 )
 def test_glcm_4d_cp(size, bins, radius, batches):
-    """ Tests the case where a batch is sent. """
     cp.random.seed(0)
-    ar_batch = cp.random.randint(0, bins, [batches, size, size, 1])
-    glcm = GLCM(radius=radius, bin_from=bins, bin_to=bins)
+    ar_batch = cp.random.randint(0, bins, [batches, size, size, 3])
+    glcm = GLCMCross(radius=radius, bin_from=bins, bin_to=bins)
     g_batch = glcm.run(ar_batch)
     g = cp.stack([glcm.run(ar) for ar in ar_batch])
     assert g.get() == pytest.approx(g_batch.get(), abs=1e-06)

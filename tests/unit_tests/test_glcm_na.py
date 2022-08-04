f456feeab4da43a132d0d83ad59181f19e4a9504
Evening <johnchangqi@hotmail.com>
2022-08-04 20:34:15 +0800

    Update to use random parameters

--- tests/unit_tests/test_glcm_na.py
+++ tests/unit_tests/test_glcm_na.py
@@ -13,11 +13,15 @@ def test_glcm_na():
     # # # # #
 
     """
-    ar_inner = np.asarray([[0, 1, 0], [1, 0, 1], [1, 1, 1]])[..., np.newaxis]
-    ar_outer = np.empty([5, 5, 1])
+    inner_size = 3
+    na_pad = 1
+    outer_size = inner_size + na_pad * 2
+    bins = 16
+    ar_inner = np.random.randint(0, bins, (inner_size, inner_size, 1))
+    ar_outer = np.empty([outer_size, outer_size, 1])
     ar_outer[:] = np.nan
-    ar_outer[1:-1, 1:-1] = ar_inner
-    glcm = GLCM(radius=0, bin_from=2, bin_to=2)
+    ar_outer[na_pad:-na_pad, na_pad:-na_pad] = ar_inner
+    glcm = GLCM(radius=0, bin_from=bins, bin_to=bins)
     ar_outer_g = glcm.run(ar_outer)
     ar_inner_g = glcm.run(ar_inner)
-    assert (ar_outer_g[1, 1, 0] == ar_inner_g[0, 0, 0]).all()
+    assert (ar_outer_g[na_pad:-na_pad, na_pad:-na_pad] == ar_inner_g).all()

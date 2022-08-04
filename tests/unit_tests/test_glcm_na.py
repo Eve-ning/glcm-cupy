972039e04319bc41b4a1e64cd54022a1a47ea190
Evening <johnchangqi@hotmail.com>
2022-08-04 21:19:26 +0800

    Add test case for NA holes

--- tests/unit_tests/test_glcm_na.py
+++ tests/unit_tests/test_glcm_na.py
@@ -39,3 +39,42 @@ def test_glcm_na_padding(inner_size, na_pad, bins):
     ar_inner_g = glcm.run(ar_inner)
     assert (ar_outer_g[na_pad:-na_pad, na_pad:-na_pad] == ar_inner_g).all()
 
+
+@pytest.mark.parametrize(
+    'size',
+    (5, 7, 9)
+)
+@pytest.mark.parametrize(
+    'holes',
+    (1, 3, 5)
+)
+def test_glcm_na_holes(size, holes):
+    """ Tests effects of NA holes in images
+
+    Examples:
+        For an image with only 1 number, for MOST combination of holes, the
+        resulting GLCM is the same as the one without holes.
+
+        0 0 0      # 0 0      0 # 0
+        0 0 0  ==  0 # 0  ==  0 # #
+        0 0 0      # 0 0      # 0 #
+
+        This is because only probability is considered, NAs are not counted in
+        the denominator.
+
+        However, if the windows spans an all NA case, then it'll be different.
+
+        To avoid this, we limit the number of random holes
+
+    """
+    bins = 4
+    hole_ixs = np.random.choice(size * size, holes, replace=False)
+    ar = np.ones((size * size, 1)) * (bins - 1)
+    ar_holes = ar.copy()
+    ar_holes[hole_ixs] = np.nan
+    ar = ar.reshape(size, size, 1)
+    ar_holes = ar_holes.reshape(size, size, 1)
+    glcm = GLCM(radius=1, bin_from=bins, bin_to=bins)
+    ar_g = glcm.run(ar)
+    ar_holes_g = glcm.run(ar_holes)
+    assert (ar_g == ar_holes_g).all()
